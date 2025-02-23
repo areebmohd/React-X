b02d7e25616d6035dca9deb5809c4daacca06b8a
areebmohd <areebmohd683@gmail.com>
2025-02-23 18:08:19 +0530

    vercel analytics added

--- X/src/main.jsx
+++ X/src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
-      <SpeedInsights />
+      <Analytics/>
     </BrowserRouter>
   </StrictMode>,
 )
