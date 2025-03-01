1b00b3c0d510c1813b9aee5190c06653a1b47259
areebmohd <areebmohd683@gmail.com>
2025-03-01 08:05:43 +0530

    about section completed

--- X/src/components/aboutSection.jsx
+++ X/src/components/aboutSection.jsx
@@ -12,6 +12,10 @@ function AboutSection() {
             <p>• Technology</p>
             <p>• Science</p>
             <p>• Fiction</p>
+            <h4>Communities</h4>
+            <a href="https://x.com/i/communities/1497313371053510658" target='_blank'>One Piece</a>
+            <a href="https://x.com/i/communities/1600302257614069762" target='_blank'>Marvel</a>
+            <a href="https://x.com/i/communities/1733132808745283911" target='_blank'>Grok</a>
             <h4>Relationship</h4>
             <p>• Single</p>
             <h4>Location</h4>
@@ -25,12 +29,19 @@ function AboutSection() {
             <p>• High School from Riverdale International School</p>
             <h4>Skills</h4>
             <p>• Frontend development using React</p>
-            <p>• Python and MySQL</p>
+            <p>• Javascript</p>
+            <p>• Python</p>
+            <p>• MySQL</p>
             <h4>Links</h4>
-            <p className='link'>xbyashu.vercel.app</p>
+            <a href="https://xbyashu.vercel.app" target='_blank'>xbyashu.vercel.app</a>
+            <a href="https://github.com/areebmohd" target='_blank'>github.com/areebmohd</a>
+            <a href="https://x.com/ashu_cdx" target='_blank'>x.com/ashu_cdx</a>
             <h4>Work History</h4>
             <p>• Frontend Developer at XXX from July 2024 to present day.</p>
             <p>• Currently Student</p>
+            <h4>Contact Info</h4>
+            <a href="https://aashu9105628720@gmail.com" target='_blank'>Email</a>
+            <a href="https://x.com/ashu_cdx" target='_blank'>x.com/ashu_cdx</a>
             <h4>Account Info</h4>
             <p>• Joined October 2024</p>
             <p>• Phone number verified</p>
